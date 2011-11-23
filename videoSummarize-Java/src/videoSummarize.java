f247a33e37c07acb78efd22d906aeb3a4aa19c1f
popcornjiver@gmail.com <popcornjiver@gmail.com@7e6415d1-4dd7-864f-1ff6-5aa03dc70836>
2011-11-23 02:43:49 +0000

    commit the simple skeleton, not sync-ing with wav at the moment

---
+++ videoSummarize-Java/src/videoSummarize.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.lang.*;
+
+import image.GoThroughImage;
+import sound.playWave.PlaySound;
+import sound.playWave.PlayWaveException;
+
+/**
+ * trigger analyzing 
+ * 
+ * 
+ */
+
+public class videoSummarize{
+	
+	 public static void main(String[] args) {
+			// get the command line parameters
+			if (args.length < 3) {
+			    System.err.println("usage: java videoSummarize [videoInput.rgb] [audioInput.wav] [percentage]");
+			    return;
+			}
+			String videofilename = args[0];
+			String audiofilename = args[1];
+			int percentage = Integer.parseInt(args[2]);
+			
+			if (percentage <= 0 || percentage >100){
+				System.err.println("percentage parameter must not <=0 and >100");
+				return;
+			}
+
+			FileInputStream videoinputStream;
+			File videofile;
+			try {
+				videofile = new File(videofilename);
+				videoinputStream = new FileInputStream(videofile);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			    return;
+			}
+			
+			GoThroughImage gti = new GoThroughImage(videoinputStream, percentage);
+			gti.filter();
+			//videoinputStream.close(); not so important for this program
+			
+			
+			/*
+			// opens the inputStream
+			FileInputStream audioinputStream;
+			try {
+			    audioinputStream = new FileInputStream(audiofilename);
+			} catch (FileNotFoundException e) {
+			    e.printStackTrace();
+			    return;
+			}
+
+			// initializes the playSound Object
+			PlaySound playSound = new PlaySound(audioinputStream);
+
+			// plays the sound
+			try {
+			    playSound.play();
+			} catch (PlayWaveException e) {
+			    e.printStackTrace();
+			    return;
+			}
+			*/
+	 }
+}
