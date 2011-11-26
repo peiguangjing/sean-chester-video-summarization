449e2281c219a5cabf621089369cef7b8da8af2b
mezentius <mezentius@7e6415d1-4dd7-864f-1ff6-5aa03dc70836>
2011-11-26 16:58:22 +0000

    Finally have the audio/video in sync for the video player.  Performant display of dynamically generated video in WPF has a lot of potential pitfalls, most of which I fell into having been accustomed to more traditional UI programming.

--- trunk/FinalProject/MainWindow.xaml.cs
+++ trunk/FinalProject/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
-using System.Windows.Media.Imaging;
 
 namespace FinalProject
 {
---
+++ trunk/VideoPlayer/Video.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Threading;
+using System.Timers;
+using System.ComponentModel;
+
+using Color = System.Drawing.Color;
+using Image = System.Windows.Controls.Image;
+using System.Diagnostics;
+
+namespace VideoPlayer
+{
+
+    /// <summary>
+    /// Interaction logic for Video.xaml
+    /// </summary>
+    public partial class Video : Image, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public VideoViewModel VideoModel { get; set; }
+
+        private static float MillisecondsPerFrame = 1000.0f / 23.97f;
+
+        private Stopwatch _stopwatch = new Stopwatch();
+        private float RunningTime { get; set; }
+
+        private WriteableBitmap BitmapSource = new WriteableBitmap(320, 240, 96, 96, System.Windows.Media.PixelFormats.Rgb24, null);
+
+        public bool OpenFile(String videoFileName, String audioFileName)
+        {
+            return VideoModel.OpenFile(videoFileName, audioFileName);
+        }
+
+        public void Play(bool StartTimer = false)
+        {
+            Source = BitmapSource;
+            VideoModel.Play();
+            _stopwatch.Start();
+            RunningTime = 0.0f;
+        }
+
+        public Video()
+        {
+            InitializeComponent();
+
+            CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
+
+            VideoModel = new VideoViewModel();
+        }
+
+        void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            if (VideoModel.IsPlaying())
+            {
+                RunningTime += _stopwatch.Elapsed.Milliseconds;
+                if (MillisecondsPerFrame <= RunningTime)
+                {
+                    //Console.WriteLine("{0}", RunningTime);
+                    RunningTime -= MillisecondsPerFrame;
+                    _stopwatch.Restart();
+                    VideoModel.OnVideoTimerTick(BitmapSource);
+                }
+                else
+                {
+                    _stopwatch.Restart();
+                }
+            }
+        }
+    }
+}
