e4547db441ac9337033182f56e7eb8782a8a5f74
mezentius <mezentius@7e6415d1-4dd7-864f-1ff6-5aa03dc70836>
2011-11-27 19:58:58 +0000

    Implemented a color difference evaluation that effectively measures the higher frequencies of a frame (ie first pass of JPEG2000 compression).  Helps in actual practice.

--- videoSummarize/src/shot/Shot.java
+++ videoSummarize/src/shot/Shot.java
@@ -16,10 +16,11 @@ public class Shot {
 
 	//Motion weight is a weight on the total motion vector distances for a frame.  The idea here is that
 	//frames with more motion are more important and shots with more motion are more important.
-	private final int MotionWeight = 10;
+	private final int MotionWeight = 100;
 	//Color weight is a weight on the sum of the color components of the average color for a frame.  The idea
 	//here is that shots that are more saturated/brighter (thus having higher RGB values) are more important.
-	private final int ColorWeight = 5000; 
+	private final int AverageColorWeight = 100;
+	private final int ColorDifferenceWeight = 1000; 
 	
 	private int ShotIndex = 0;
 	private float ShotImportance = 0.0f;
@@ -68,16 +69,18 @@ public class Shot {
 	public void CalculateShotImportance(Vector<AnalyzedFrame> frames)
 	{
 		//Apply weights given the information stored in each AnalyzedFrame in the sequence from FrameLowerIndex to FrameUpperIndex
-		float motionContribution = 0.0f;
-		float colorContribution = 0.0f;
+		double motionContribution = 0.0f;
+		double colorContribution = 0.0f;
+		double colorDifferenceContribution = 0.0f;
 		for(int index = (int)FrameLowerIndex; index <= FrameUpperIndex; index++)
 		{
 			motionContribution += frames.elementAt(index).TotalMotion();
 			colorContribution += frames.elementAt(index).TotalColor();
 		}
 		motionContribution /= MotionWeight;
-		colorContribution /= ColorWeight;
-		ShotImportance = motionContribution + colorContribution;;
+		colorContribution /= AverageColorWeight;
+		colorDifferenceContribution /= ColorDifferenceWeight;
+		ShotImportance =(float) (motionContribution + colorContribution + colorDifferenceContribution);
 	}
 	
 	public void OutputShot(RandomAccessFile source, FileOutputStream output)
