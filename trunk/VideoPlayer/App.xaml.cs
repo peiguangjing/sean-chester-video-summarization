693f330ee14dd2ffdc8e0e568dbd3b9519c85cff
mezentius <mezentius@7e6415d1-4dd7-864f-1ff6-5aa03dc70836>
2011-12-05 19:23:22 +0000

    Final updates.

--- trunk/VideoPlayer/App.xaml.cs
+++ trunk/VideoPlayer/App.xaml.cs
@@ -4,7 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
-
+using System.IO;
 namespace VideoPlayer
 {
     /// <summary>
@@ -12,5 +12,45 @@ namespace VideoPlayer
     /// </summary>
     public partial class App : Application
     {
+        private bool ProcessCommandLine(StartupEventArgs e)
+        {
+            string videoFileName;
+            string audioFileName;
+            if (e.Args.Length == 2)
+            {
+                videoFileName = e.Args[0];
+                if (File.Exists(videoFileName))
+                {
+                    audioFileName = e.Args[1];
+                    if (File.Exists(audioFileName))
+                    {
+                        if (App.Current.MainWindow.IsLoaded)
+                        {
+
+                        }
+                        else
+                        {
+
+
+                        }
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Could not find audio file.  Please check that your specified path is correct.  Exiting application.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Could not find video file.  Please check that your specified path is correct.  Exiting application.");
+                }
+            }
+            else if( e.Args.Length > 2)
+            {
+                MessageBox.Show("You have specified too many commandline parameters.  Exiting application.");
+            }
+            return false;
+        }
+
     }
 }
